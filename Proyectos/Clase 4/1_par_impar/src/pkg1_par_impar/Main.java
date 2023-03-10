d13c3571eaf3178ec5e178949701dee96a14b6d5
JheX <nicorodriguez242003@gmail.com>
2023-03-10 19:04:30 -0300

    commite 1

---
+++ Proyectos/Clase 4/1_par_impar/src/pkg1_par_impar/Main.java
@@ -0,0 +1,25 @@
+/*
+    Crear un programa que dado un número determine si es par o impar.
+ */
+package pkg1_par_impar;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner (System.in);
+        int n;
+        String c;
+        System.out.println("Ingrese números");
+        do {
+            n = leer.nextInt();
+            if (n % 2 == 0) {
+                System.out.println(n + " es par");
+            } else {
+                System.out.println(n + " es impar");
+            }
+            System.out.println("¿Fin? (S/N)");
+            c = leer.next();
+        } while (c.equalsIgnoreCase("s") == false);
+    }
+}
