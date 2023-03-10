59dd7524d7a5d0214b49cd3c14ff317ce14c4734
JheX <nicorodriguez242003@gmail.com>
2023-03-10 19:20:54 -0300

    commite 1

--- Proyectos/Clase 4/Guia/1_guia/src/pkg1_guia/Main.java
+++ Proyectos/Clase 4/Guia/1_guia/src/pkg1_guia/Main.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    Implementar un programa que le pida dos números enteros al usuario y 
+    determine si ambos o alguno de ellos es mayor a 25.
  */
 package pkg1_guia;
 
--- Proyectos/Clase 4/Guia/2_guia/src/pkg2_guia/Main.java
+++ Proyectos/Clase 4/Guia/2_guia/src/pkg2_guia/Main.java
@@ -1,6 +1,6 @@
 /*
- Considera que estás desarrollando una web para una empresa que fabrica motores (suponemos que se trata del tipo de motor de una bomba para mover fluidos). 
- Definir una variable tipoMotor y permitir que el usuario ingrese un valor entre 1 y 4. El programa debe mostrar lo siguiente:
+    Considera que estás desarrollando una web para una empresa que fabrica motores (suponemos que se trata del tipo de motor de una bomba para mover fluidos). 
+    Definir una variable tipoMotor y permitir que el usuario ingrese un valor entre 1 y 4. El programa debe mostrar lo siguiente:
 o	Si el tipo de motor es 1, mostrar un mensaje indicando “La bomba es una bomba de agua”.
 o	Si el tipo de motor es 2, mostrar un mensaje indicando “La bomba es una bomba de gasolina”.
 o	Si el tipo de motor es 3, mostrar un mensaje indicando “La bomba es una bomba de hormigón”.
@@ -14,10 +14,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner leer = new Scanner (System.in);
-        
+
         System.out.println("Ingrese el tipo de motor (1 al 4)");
         int tipoMotor = leer.nextInt();
-        
+
         switch (tipoMotor) {
             case 1:
                 System.out.println("La bomba es una bomba de agua");
